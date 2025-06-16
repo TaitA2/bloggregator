244ccd47bfb7aea2425a20a4600545aad644893a
Adam PARCH <adam.tait617@gmail.com>
2025-06-16 13:21:23 +0100

    reformat tests

--- main_test.go
+++ main_test.go
@@ -8,25 +8,21 @@ import (
 	"github.com/TaitA2/bloggregator/internal/config"
 )
 
-func TestMain(t *testing.T) {
+func TestSetUser(t *testing.T) {
 	conf, err := config.Read()
 	if err != nil {
 		return
 	}
 	names := []string{"TestName", "OtherName", "FirstLast"}
+	homedir, err := os.UserHomeDir()
 	for _, name := range names {
-		if !testSetUser(name, conf) {
+		conf.SetUser(name)
+		content, err := os.ReadFile(homedir + "/.gatorconfig.json")
+		if err != nil {
+			t.Errorf("Failed to read config file while setting user name to: %s", name)
+		}
+		if string(content) != fmt.Sprintf("{\"db_url\":\"postgres://example\",\"current_user_name\":\"%s\"}", name) {
 			t.Errorf("Failed to set username to %s", name)
 		}
 	}
-}
-
-func testSetUser(name string, conf config.Config) bool {
-	conf.SetUser(name)
-	homedir, err := os.UserHomeDir()
-	if err != nil {
-		return false
-	}
-	content, err := os.ReadFile(homedir + "/.gatorconfig.json")
-	return string(content) == fmt.Sprintf("{\"db_url\":\"postgres://example\",\"current_user_name\":\"%s\"}", name)
 }
